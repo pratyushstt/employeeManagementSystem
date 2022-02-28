be41f83482eb1ef00ccbe1c270168aac284d23cb
Pratyush Sharma <99169389+pratyushstt@users.noreply.github.com>
2022-02-28 19:04:35 +0530

    Add files via upload

---
+++ employee.sql
@@ -0,0 +1,6 @@
+-- CREATING table Department that will hold the department name with unique id representing them
+CREATE TABLE department(
+    id int AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    
+);
